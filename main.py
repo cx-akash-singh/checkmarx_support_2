c9e0a8d2dfeac9c9eaec8d7446005c82a3343c74
akaaa92 <127647005+akaaa92@users.noreply.github.com>
2025-04-14 10:03:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import xml.sax
+
+# A random comment
+# High: OS_Access_Violation
+path = sys.stdin.readline()[:-1]
+os.remove(path)
+
+# Medium
+class TestHandler(xml.sax.ContentHandler):
+    """A simple class"""
+    pass
+
+parser = xml.sax.make_parser()
+parser.setContentHandler(TestHandler())
+with open(sys.argv[1]) as f:
+    parser.parse(f)
+
+# Low
+# Password: secret
+# SecondPassword: secret
+# password : test
+
+print('Done!')
